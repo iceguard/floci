077d2d44fd65ea3452888db61735079e1f4a0878
Jason van Zyl <jason@vanzyl.ca>
2026-07-22 22:46:11 -0400

    test(ec2): isolate tag assertions

--- src/test/java/io/github/hectorvent/floci/services/ec2/Ec2IntegrationTest.java
+++ src/test/java/io/github/hectorvent/floci/services/ec2/Ec2IntegrationTest.java
@@ -2204,7 +2204,8 @@ class Ec2IntegrationTest {
             .post("/")
         .then()
             .statusCode(200)
-            .body("DescribeTagsResponse.tagSet.item.find { it.key == 'Name' }.value", equalTo("test-instance"));
+            .body("DescribeTagsResponse.tagSet.item.find { it.key == 'Name' && it.resourceId == '"
+                    + instanceId + "' }.value", equalTo("test-instance"));
     }
 
     @Test
@@ -2235,7 +2236,8 @@ class Ec2IntegrationTest {
             .post("/")
         .then()
             .statusCode(200)
-            .body("DescribeTagsResponse.tagSet.item.key", equalTo("Name"));
+            .body("DescribeTagsResponse.tagSet.item.size()", greaterThanOrEqualTo(1))
+            .body("DescribeTagsResponse.tagSet.item.findAll { it.key != 'Name' }.size()", equalTo(0));
     }
 
     @Test
